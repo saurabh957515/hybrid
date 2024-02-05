60bc7e45a9d289cd94879548dbb8fa83623e27d2
Kalp Adhwaryu <team@vardaam.com>
2024-02-05 12:31:01 +0530

    env server setup

--- server/app.js
+++ server/app.js
@@ -1,10 +1,18 @@
+require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const indexRouter = require("./routes/index");
 const port = process.env.PORT || "5000";
 app.use(express.static("build"));
 app.use("/", indexRouter);
 
+// mongoose.connect(process.env.DATABASE_URL);
+// const db = mongoose.connection;
+// db.on("error", (error) => console.log(`MongoDB connection error: ${error}`));
+// db.once("open", () => {
+//   console.log("Connected to mongoose");
+// });
 app.listen(port, () => {
-  `local hosting running on ${port}`;
+  console.log(`local hosting running on ${port}`);
 });
